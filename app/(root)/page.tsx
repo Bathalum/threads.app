d81949ffddef981c19f1625647e90ca1b232eee2
Bathalum <124691760+Bathalum@users.noreply.github.com>
2024-01-10 20:23:00 +0800

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
  export default async function Home() {
-  const result = await fetchPosts(1, 30);
+  const result = await fetchPosts(1, 10);
   const user = await currentUser();
 
   console.log(result)
