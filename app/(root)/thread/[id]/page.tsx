70a6bb7e415e8db3004611625c907f112b446891
Bathalum <124691760+Bathalum@users.noreply.github.com>
2024-01-20 07:53:27 +0800

    test update

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -56,6 +56,9 @@ const Page = async ({ params }: { params: {id: string}}) => {
                     />
                 ))}
             </div>
+
+            <div>
+            </div>
         </section>
     )
 }
